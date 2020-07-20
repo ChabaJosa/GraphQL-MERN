f06f520d873eeaaf58440681380526bbefc9f645
ChabaJosa <chabagjg@gmail.com>
2020-07-20 13:43:02 -0400

    Needed minor change to have the playground respond to a simple call

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 
 const typeDefs = gql`
-    type Query{
-        sayHi: String!
-    }
+  type Query {
+    sayHi: String!
+  }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => {
-      "Hello World!";
-    },
-  },
+    sayHi: () => "Hello World!"
+  }
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
-server.listen({port: 5000})
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
-    })
+server.listen({ port: 5000 }).then((res) => {
+  console.log(`Server running at ${res.url}`);
+});
